9088e0a7881e01a591d3aa3bc39698f75d7bb6ab
Mr.Adam <adamn2742@gmail.com>
2026-03-22 23:14:15 +0700

    modified:   .env.example 	modified:   app/Providers/AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-            Schema::defaultStringLength(191);
+          
 
     }
 }
